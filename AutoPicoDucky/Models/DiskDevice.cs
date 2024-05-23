9c7e0f70ac72f5464148ded0656e7b02605bb0ef
QAEZZ <russian.devv@gmail.com>
2024-05-23 12:11:59 -0500

    added DiskChild

--- AutoPicoDucky/Models/DiskDevice.cs
+++ AutoPicoDucky/Models/DiskDevice.cs
@@ -1,5 +1,14 @@
-
 namespace AutoPicoDucky.Models;
+
+public class DiskChild {
+    public string Name { get; set; }
+    public string MajMin { get; set; }
+    public bool Rm { get; set; }
+    public string Size { get; set; }
+    public bool Ro { get; set; }
+    public string Type { get; set; }
+    public List<string>? Mountpoints { get; set; }
+}
 
 public class Disk
 {
@@ -10,5 +19,5 @@ public class Disk
     public bool Ro { get; set; }
     public string Type { get; set; }
     public List<string>? Mountpoints { get; set; }
-    public List<Disk>? Children { get; set; }
+    public List<DiskChild>? Children { get; set; }
 }
